36bf9639863af5b3f47254d6b8fef3798f6a2eaa
haoyu2 <haoyuumb@gmail.com>
2021-07-14 21:30:31 -0400

    hw3

--- plot.py
+++ plot.py
@@ -10,28 +10,28 @@ with open('points.csv', newline='') as csvfile:
         y.append(float(row[1]))
 
 
-# x1 = []
-# y1 = []
-# with open('pointsCon.csv', newline='') as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#     for row in spamreader:
-#         x1.append(float(row[0]))
-#         y1.append(float(row[1]))
-
-
-x2 = []
-y2 = []
-with open('sortedPoints.csv', newline='') as csvfile:
+x1 = []
+y1 = []
+with open('pointsCon.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        x2.append(float(row[0]))
-        y2.append(float(row[1]))
+        x1.append(float(row[0]))
+        y1.append(float(row[1]))
+
+
+# x2 = []
+# y2 = []
+# with open('sortedPoints.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+#     for row in spamreader:
+#         x2.append(float(row[0]))
+#         y2.append(float(row[1]))
 
 
 
 plt.scatter(x, y, marker='o', color = 'r')
-plt.plot(x2,y2, label='Polar angle points line')
-# plt.plot(x1,y1, label='Convex hull')
+# plt.plot(x2,y2, label='Polar angle points line')
+plt.plot(x1,y1, label='Convex hull')
 plt.legend()
 plt.savefig(f'plot_{len(x)}.png')
 plt.show()
